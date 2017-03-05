e0e0e80c26fcaa7261ee29f780b838f91038dad6
erikmura44 <erikmura44@gmail.com>
2017-03-05 10:55:29 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  declarations: [ AppComponent, NavbarComponent ],
+  imports: [ BrowserModule, FormsModule, HttpModule ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
